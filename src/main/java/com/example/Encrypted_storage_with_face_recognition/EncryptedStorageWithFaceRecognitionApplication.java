77e7ff84fe7995e92132cf4c41e1fabcff09e403
KLINY1337 <semyon.chernomurov@gmail.com>
2023-02-22 14:00:53 +0300

    initial commit

---
+++ src/main/java/com/example/Encrypted_storage_with_face_recognition/EncryptedStorageWithFaceRecognitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Encrypted_storage_with_face_recognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptedStorageWithFaceRecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptedStorageWithFaceRecognitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Encrypted_storage_with_face_recognition/EncryptedStorageWithFaceRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Encrypted_storage_with_face_recognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptedStorageWithFaceRecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
