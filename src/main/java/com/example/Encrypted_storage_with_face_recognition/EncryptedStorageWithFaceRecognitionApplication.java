8238b5f8e8ffc3e8c6611ea888f8a3edbe0e50ba
KLINY1337 <semyon.chernomurov@gmail.com>
2023-03-06 11:23:29 +0300

    added preparation of MVC architecture. dependencies: 1) com.github.sarxos.webcam-capture, version: 0.3.12

--- src/main/java/com/example/Encrypted_storage_with_face_recognition/EncryptedStorageWithFaceRecognitionApplication.java
+++ src/main/java/com/example/Encrypted_storage_with_face_recognition/EncryptedStorageWithFaceRecognitionApplication.java
@@ -1,13 +1,22 @@
 package com.example.Encrypted_storage_with_face_recognition;
 
+import com.github.sarxos.webcam.Webcam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class EncryptedStorageWithFaceRecognitionApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(EncryptedStorageWithFaceRecognitionApplication.class, args);
+		Webcam webcam = Webcam.getDefault();
+		webcam.open();
+		ImageIO.write(webcam.getImage(),"PNG", new File("hello-world.png"));
+		webcam.close();
 	}
 
 }
