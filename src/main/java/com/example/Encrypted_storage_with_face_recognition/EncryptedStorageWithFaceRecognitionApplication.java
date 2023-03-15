d47810a5969d2f8ac7b99c03645de86b7194e1c5
KLINY1337 <semyon.chernomurov@gmail.com>
2023-03-15 20:52:30 +0300

    Added Encryption module (com.example.Encrypted_storage_with_face_recognition.Model.Modules.File.Encryption.Encryptor)

--- src/main/java/com/example/Encrypted_storage_with_face_recognition/EncryptedStorageWithFaceRecognitionApplication.java
+++ src/main/java/com/example/Encrypted_storage_with_face_recognition/EncryptedStorageWithFaceRecognitionApplication.java
@@ -1,18 +1,28 @@
 package com.example.Encrypted_storage_with_face_recognition;
 
 import com.example.Encrypted_storage_with_face_recognition.Model.Modules.Face.Recognition.FaceRecognizer;
+import com.example.Encrypted_storage_with_face_recognition.Model.Modules.File.Encryption.Encryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.File;
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EncryptedStorageWithFaceRecognitionApplication {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
 		SpringApplication.run(EncryptedStorageWithFaceRecognitionApplication.class, args);
 		FaceRecognizer faceRecognizer = new FaceRecognizer();
 		System.out.println(faceRecognizer.detectFace());
+
+		System.out.println(Arrays.toString(Encryptor.encrypt(new File("LICENSE.txt"))));
 	}
 
 }
--- src/main/java/com/example/Encrypted_storage_with_face_recognition/Model/Modules/File/Encryption/Encryptor.java
+++ src/main/java/com/example/Encrypted_storage_with_face_recognition/Model/Modules/File/Encryption/Encryptor.java
@@ -5,13 +5,60 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.crypto.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 @Setter
 @Getter
 @Slf4j
 public class Encryptor {
-    public Encryptor(){
+    public static byte[] encrypt(File file)  {
+        try {
+            byte[] plainText  = Files.readAllBytes(Path.of(file.getPath()));
 
+            Cipher cipher = getCipher();
+
+            return cipher.doFinal(plainText);
+
+        } catch (IllegalBlockSizeException | IOException | BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    private static Cipher getCipher() {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 
+            SecretKey secretKey = getSecretKey();
+
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+            return cipher;
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    private static SecretKey getSecretKey() {
+
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+
+            SecureRandom secureRandom = new SecureRandom();
+
+            int keyBitSize = 256;
+
+            keyGenerator.init(keyBitSize, secureRandom);
+
+            return keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
