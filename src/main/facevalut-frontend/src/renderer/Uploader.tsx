06d66f36441e8aeaea6389ef7f689810a73943ab
lexa2hk <alesha.smolnikoff@gmail.com>
2023-03-19 19:36:34 +0300

    Removed submodule, frontend now is in Repository

---
+++ src/main/facevalut-frontend/src/renderer/Uploader.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+function FileUpload() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!selectedFile) {
+      alert('Please select a file');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+      const response = await fetch('/api/upload', {
+        method: 'POST',
+        body: formData,
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedFile(event.target.files?.[0] || null);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Select file to upload:
+        <input type="file" onChange={handleFileChange} />
+      </label>
+      <br />
+      <button type="submit">Upload</button>
+    </form>
+  );
+}
+
+export default FileUpload;
