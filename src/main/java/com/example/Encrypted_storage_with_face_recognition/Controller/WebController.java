4a08f7b62e3a567376b11a1e737cd4164a3828f2
lexa2hk <alesha.smolnikoff@gmail.com>
2023-03-19 19:11:57 +0300

    Added WebController isKeystoreExist endpoint (com.example.Encrypted_storage_with_face_recognition.Controller.WebController)

--- src/main/java/com/example/Encrypted_storage_with_face_recognition/Controller/WebController.java
+++ src/main/java/com/example/Encrypted_storage_with_face_recognition/Controller/WebController.java
@@ -1,25 +1,36 @@
 package com.example.Encrypted_storage_with_face_recognition.Controller;
 
 
+import com.example.Encrypted_storage_with_face_recognition.Model.Modules.File.Create.KeyStore.KeyStoreService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+
 @RestController
 @RequestMapping(value = "/")
+@Slf4j
 public class WebController {
 
-    @RequestMapping(value = "/{page}")
-    public String page(@PathVariable String page) {
-        return page;
+    @Autowired
+    private KeyStoreService keyStoreService;
+
+    @RequestMapping(value = "/isKeyStoreExist")
+    public ResponseEntity<?> page() {
+        try {
+            return ResponseEntity.ok(new HashMap<String,Boolean>(){{put("isKeyStoreExist",keyStoreService.isKeyStoreExist());}});
+        }
+        catch (Exception exception){
+            return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
+        }
     }
 
-    @RequestMapping(value = "")
-    public ModelAndView index() {
-        ModelAndView mav =new ModelAndView("index.html");
-        return mav;
-    }
 
 }
